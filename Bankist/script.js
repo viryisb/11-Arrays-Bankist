3e9ffa078095b0fc7d9eb142bfb80876867ee359
Virginia <vbernasconi@altimetrik.com>
2022-10-22 19:26:41 -0300

    implement close account feature

--- Bankist/script.js
+++ Bankist/script.js
@@ -145,6 +145,26 @@ btnLogin.addEventListener('click', function (e) {
     updateUI(currentAccount);
   }
 });
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+
+    //Delete account
+    accounts.splice(index, 1);
+
+    //Hide UI
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputCloseUsername.value = '';
+});
+
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
   const amount = Number(inputTransferAmount.value);
