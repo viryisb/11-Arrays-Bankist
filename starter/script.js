08d72fc7bc7c6a1ee5a2305f7a54337b58e6808f
Virginia <vbernasconi@altimetrik.com>
2022-10-23 23:22:21 -0300

    array methods practice

--- starter/script.js
+++ starter/script.js
@@ -129,7 +129,7 @@ console.log(movements.includes(-130));
 //Condition
 console.log(movements.some(mov => mov === -130));
 const anyDeposit = movements.some(mov => mov > 1500);
-console.log(anyDeposits);
+console.log(anyDeposit);
 
 //Every
 console.log(movements.every(mov => mov > 0));
@@ -218,5 +218,69 @@ labelBalance.addEventListener('click', function () {
 
   console.log(movementsUI);
 
-  const movementsUI2 = [...document.querySelectorAlll('.movements__value')];
+  //const movementsUI2 = [...document.querySelectorAlll('.movements__value')];
 });
+
+const bankDepositSum = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov > 0)
+  .reduce((sum, cur) => sum + cur);
+console.log('bankDepositSum', bankDepositSum);
+
+//2
+/* const numDeposits1000 = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov >= 1000).length; */
+const numDeposits1000 = accounts
+  .flatMap(acc => acc.movements)
+  /* .reduce((count, cur) => (cur >= 1000 ? count + 1 : count), 0); */
+  .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0);
+console.log(numDeposits1000);
+
+//Prefixed ++ operator
+let a = 10;
+console.log(++a);
+console.log(a);
+
+//3 Reduce
+/* const { deposits, withdrawals } = accounts
+  .flatMap(acc => acc.movements)
+  .reduce(
+    (sums, cur) => {
+      cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+      return sums;
+    },
+    { deposits: 0, withdrawals: 0 }
+  ); */
+
+const { deposits, withdrawals } = accounts
+  .flatMap(acc => acc.movements)
+  .reduce(
+    (sums, cur) => {
+      /*  cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur); */
+      sums[cur > 0 ? 'deposits' : 'withdrawals'];
+      return sums;
+    },
+    { deposits: 0, withdrawals: 0 }
+  );
+
+console.log('d w', deposits, withdrawals);
+
+//4
+// this is a nice title -> This Is a Nice Title
+const convertTitleCase = function (title) {
+  const capitalize = str => str[0].toUpperCase() + str.slice(1);
+  const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word =>
+      exceptions.includes(word) ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join(' ');
+  return capitalize(titleCase);
+};
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG title but not too long'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE'));
